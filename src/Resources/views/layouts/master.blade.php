3ed0b7e40a7186d5eb1c1a455a84b72a99ad80df
Hasan <hcivelek@kuzen.net>
2019-03-26 15:56:54 +0300

    font awesome added

--- src/Resources/views/layouts/master.blade.php
+++ src/Resources/views/layouts/master.blade.php
@@ -4,9 +4,14 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Categories</title>
- 
-       <link rel="stylesheet" href="{{ package_asset('css/app.css') }}">
+        <title>Categories</title>
+    
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
+                
+        <link rel="stylesheet" href="{{ package_asset('css/app.css') }}">
 
     </head>
     <body>
